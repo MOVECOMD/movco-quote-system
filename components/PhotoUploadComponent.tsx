3d872e2b918e1b9858cb2fb5091c2ad7a2143d82
Zachary <zachkench@gmail.com>
2026-02-12 16:43:36 +0000

    Point frontend to Render backend

--- components/PhotoUploadComponent.tsx
+++ components/PhotoUploadComponent.tsx
@@ -114,7 +114,7 @@ export default function PhotoUploadComponent() {
     setResult(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/analyze', {
+      const response = await fetch('https://movco-api.onrender.com/analyze', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
